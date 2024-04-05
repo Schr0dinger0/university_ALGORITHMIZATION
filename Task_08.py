7bb584af6fc08f7fe60c32812a4ecc92202c57c8
korya <Koryazov.dima@gmail.com>
2024-04-05 21:09:55 +0300

    Tasks

--- Task_08.py
+++ Task_08.py
@@ -1,6 +1,4 @@
-# Функция для определения элементов, встречающихся по одному разу в последовательности
 def find_unique_elements(lst):
-    # Создаем словарь для подсчета количества встреч каждого элемента
     element_count = {}
     for num in lst:
         if num in element_count:
@@ -8,12 +6,10 @@ def find_unique_elements(lst):
         else:
             element_count[num] = 1
 
-    # Выводим элементы, которые встречаются по одному разу
     unique_elements = [key for key, value in element_count.items() if value == 1]
     return unique_elements
 
 
-# Ввод количества элементов и списка с клавиатуры
 try:
     count = int(input("Введите количество элементов: "))
     if count <= 0:
@@ -25,7 +21,6 @@ try:
             num = int(input(f"Элемент {i + 1}: "))
             lst.append(num)
 
-        # Поиск уникальных элементов
         unique_elements = find_unique_elements(lst)
         if unique_elements:
             print("Элементы, которые встречаются по одному разу в последовательности:", unique_elements)
